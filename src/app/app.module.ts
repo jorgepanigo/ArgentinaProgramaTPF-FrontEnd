6f33acc36a8492953e28cacf948afa595442bbed
jorgepanigo <jorgepanigo@gmail.com>
2022-03-11 16:10:29 -0300

    Generacion de componentes basicos generada

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,36 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { HeaderComponent } from './components/header/header.component';
+import { AboutmeComponent } from './components/aboutme/aboutme.component';
+import { EducationComponent } from './components/education/education.component';
+import { ExperienceComponent } from './components/experience/experience.component';
+import { SkillsComponent } from './components/skills/skills.component';
+import { ProyectsComponent } from './components/proyects/proyects.component';
+import { EducationItemComponent } from './components/education/education-item/education-item.component';
+import { ExperienceItemComponent } from './components/experience/experience-item/experience-item.component';
+import { SkillItemComponent } from './components/skills/skill-item/skill-item.component';
+import { ProyectItemComponent } from './components/proyects/proyect-item/proyect-item.component';
+import { LoginComponent } from './components/login/login.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    HeaderComponent,
+    AboutmeComponent,
+    EducationComponent,
+    ExperienceComponent,
+    SkillsComponent,
+    ProyectsComponent,
+    EducationItemComponent,
+    ExperienceItemComponent,
+    SkillItemComponent,
+    ProyectItemComponent,
+    LoginComponent,
+
   ],
   imports: [
     BrowserModule,
