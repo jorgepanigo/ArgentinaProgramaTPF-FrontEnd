cc6ed400f4461c05d5175ec0d98fd6a891b0d90b
jorgepanigo <jorgepanigo@gmail.com>
2022-03-19 23:37:56 -0300

    estructura html de proyectos generada

--- src/app/components/proyects/proyect-item/proyect-item.component.ts
+++ src/app/components/proyects/proyect-item/proyect-item.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { LoginService } from 'src/app/services/login/login.service';
 
 @Component({
   selector: 'app-proyect-item',
@@ -7,9 +8,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProyectItemComponent implements OnInit {
 
-  constructor() { }
+  constructor(private ls:LoginService) { }
 
   ngOnInit(): void {
   }
-
+  
+  isLoged():boolean{
+    return this.ls.isLoged();
+  }
 }
--- src/app/components/proyects/proyects.component.ts
+++ src/app/components/proyects/proyects.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { LoginService } from 'src/app/services/login/login.service';
 
 @Component({
   selector: 'app-proyects',
@@ -7,9 +8,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProyectsComponent implements OnInit {
 
-  constructor() { }
+  constructor(private ls:LoginService) { }
 
   ngOnInit(): void {
   }
-
+  
+  isLoged():boolean{
+    return this.ls.isLoged();
+  }
 }
